b295d54e4ad90d626c43077a31e1f7cf2aead5a5
emisner15 <em1909@humboldt.edu>
2018-03-03 16:14:10 -0800

    First commit

---
+++ Server Side/MainMenu.php
@@ -0,0 +1,24 @@
+<!DOCTYPE> 
+
+<html>
+
+<body>
+
+<head>
+    <title> Main Menu </title>
+</head>
+
+    <div>
+        <form action = "NewRental.php"
+	     method= "post">
+	    <fieldset >
+	        <input type="submit" name="NRental" value="NewRental" /><br />
+		<input type="submit" name="ViewInv" value="View/Edit Inventory" /><br />
+		<input type="submit" name="ViewVen" value="View/Edit Vendors" /><br />
+	    </fieldset>
+	</form>
+    </div>
+
+</body>
+
+</html>
---
+++ Server Side/NewRental.php
@@ -0,0 +1,34 @@
+<!DOCTYPE> 
+
+<html>
+
+<body>
+
+<head>
+    <title> Main Menu </title>
+</head>
+
+    <div>
+        <form action = "NewRental.php"
+	     method= "post">
+	    <fieldset >
+                <table style="width:100%">
+                <tr>
+                	<th>Item</th>
+                	<th>Ammount Left</th> 
+                	<th>Price</th>
+                </tr>
+                <tr>
+    			<td>Tents</td>
+    			<td>10</td>
+			<td>$20</td>
+		</tr>
+		</table>
+	    </fieldset>
+	</form>
+    </div>
+
+</body>
+
+</html>
+
